23ba7261ba6d304cc4a5ff5d17aac9faf2e0a271
Nikita Bautin <alliancesuck13@yandex.ru>
2024-08-20 20:03:38 +0700

    реализовал логику фильтра пересадок

--- src/App/redux/store.js
+++ src/App/redux/store.js
@@ -5,7 +5,11 @@ const initialState = {
   searchID: "",
   ticketList: [],
   filtredTicketList: [],
-  transferFilter: "ALL", // ALL, 0_TRANSFERS, 1_TRANSFER, 2_TRANSFER, 3_TRANSFER
+  filterIsAll: true,
+  filterIsNoTransfers: true,
+  filterIsOneTransfer: true,
+  filterIsTwoTransfers: true,
+  filterIsThreeTransfers: true,
   priceFilter: "CHEAP", // CHEAP, FAST, OPTIMAL
   isLoading: false,
   hasError: false,
@@ -13,15 +17,111 @@ const initialState = {
 
 const reducer = (state = initialState, action = undefined) => {
   switch (action.type) {
-    case "LOAD_TICKETS":
-      return { ...state, ticketList: action.payload.ticketList };
-    case "LOAD_FILTRED_TICKETS":
-      console.log(action.payload.filtredTickets);
-      return { ...state, filtredTicketList: action.payload.filtredTickets };
-    case "LOAD_SEARCH_ID":
+    case "LOAD_SEARCH_ID": // загрузка айди поиска
       return { ...state, searchID: action.payload.searchID };
-    case "GET_ERROR":
+
+    case "LOAD_TICKETS": // загрузка тикетов
+      return { ...state, ticketList: action.payload.ticketList };
+
+    case "LOAD_FILTRED_TICKETS": // отображение только 5 отфильтрованных тикетов
+      return { ...state, filtredTicketList: action.payload.filtredTickets };
+
+    case "FILTER_ALL": // все тикеты
+      if (state.filterIsAll) {
+        return {
+          ...state,
+          filterIsAll: !state.filterIsAll,
+          filterIsNoTransfers: false,
+          filterIsOneTransfer: false,
+          filterIsTwoTransfers: false,
+          filterIsThreeTransfers: false,
+        };
+      }
+      return {
+        ...state,
+        filterIsAll: !state.filterIsAll,
+        filterIsNoTransfers: true,
+        filterIsOneTransfer: true,
+        filterIsTwoTransfers: true,
+        filterIsThreeTransfers: true,
+      };
+
+    case "FILTER_TRANSFER": // тикеты в зависимости от фильтров
+      switch (Object.keys(action.payload)[0]) {
+        case "0_TRANSFERS": // без пересадок
+          if (
+            !state.filterIsNoTransfers &&
+            !state.filterIsAll &&
+            state.filterIsOneTransfer &&
+            state.filterIsTwoTransfers &&
+            state.filterIsThreeTransfers
+          ) {
+            return { ...state, filterIsAll: true, filterIsNoTransfers: true };
+          }
+
+          return {
+            ...state,
+            filterIsAll: false,
+            filterIsNoTransfers: !state.filterIsNoTransfers,
+          };
+
+        case "1_TRANSFER": // 1 пересадка
+          if (
+            !state.filterIsOneTransfer &&
+            !state.filterIsAll &&
+            state.filterIsNoTransfers &&
+            state.filterIsTwoTransfers &&
+            state.filterIsThreeTransfers
+          ) {
+            return { ...state, filterIsAll: true, filterIsOneTransfer: true };
+          }
+
+          return {
+            ...state,
+            filterIsAll: false,
+            filterIsOneTransfer: !state.filterIsOneTransfer,
+          };
+
+        case "2_TRANSFERS": // 2 пересадки
+          if (
+            !state.filterIsTwoTransfers &&
+            !state.filterIsAll &&
+            state.filterIsNoTransfers &&
+            state.filterIsOneTransfer &&
+            state.filterIsThreeTransfers
+          ) {
+            return { ...state, filterIsAll: true, filterIsTwoTransfers: true };
+          }
+
+          return {
+            ...state,
+            filterIsAll: false,
+            filterIsTwoTransfers: !state.filterIsTwoTransfers,
+          };
+
+        case "3_TRANSFERS": // 3 пересадки
+          if (
+            !state.filterIsThreeTransfers &&
+            !state.filterIsAll &&
+            state.filterIsNoTransfers &&
+            state.filterIsOneTransfer &&
+            state.filterIsTwoTransfers
+          ) {
+            return { ...state, filterIsAll: true, filterIsThreeTransfers: true };
+          }
+
+          return {
+            ...state,
+            filterIsAll: false,
+            filterIsThreeTransfers: !state.filterIsThreeTransfers,
+          };
+
+        default:
+          return { ...state };
+      }
+    case "GET_ERROR": // ошибка загрузки
       return { ...state, hasError: action.payload.hasError };
+
     default:
       return state;
   }
--- src/components/TransferFilter/TransferFilter.jsx
+++ src/components/TransferFilter/TransferFilter.jsx
@@ -1,19 +1,34 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import styles from "./TransferFilter.module.scss";
 
 export default function TransferFilter() {
-  const transferFilter = useSelector((state) => {
-    return state.transferFilter;
+  const filterIsAll = useSelector((state) => {
+    return state.filterIsAll;
+  });
+  const filterIsNoTransfers = useSelector((state) => {
+    return state.filterIsNoTransfers;
+  });
+  const filterIsOneTransfer = useSelector((state) => {
+    return state.filterIsOneTransfer;
+  });
+  const filterIsTwoTransfers = useSelector((state) => {
+    return state.filterIsTwoTransfers;
+  });
+  const filterIsThreeTransfers = useSelector((state) => {
+    return state.filterIsThreeTransfers;
   });
 
-  let isAllChecked = true;
+  const dispatch = useDispatch();
 
-  if (transferFilter === "ALL") {
-    isAllChecked = true;
-  } else {
-    isAllChecked = false;
-  }
+  const onChangeAll = () => {
+    dispatch({ type: "FILTER_ALL" });
+  };
+
+  const onChangeTransfers = (e) => {
+    const checkboxID = e.target.id;
+    dispatch({ type: "FILTER_TRANSFER", payload: { [checkboxID]: e.target.checked } });
+  };
 
   return (
     <div className={styles.TransferFilter}>
@@ -24,32 +39,57 @@ export default function TransferFilter() {
             <input
               className={styles["TransferFilter-checkbox"]}
               type="checkbox"
-              defaultChecked={isAllChecked}
+              onChange={onChangeAll}
+              checked={filterIsAll}
             />
             <span>Все</span>
           </label>
         </li>
         <li className={styles["TransferFilter-item"]}>
           <label className={styles["TransferFilter-label"]}>
-            <input className={styles["TransferFilter-checkbox"]} type="checkbox" />
+            <input
+              id="0_TRANSFERS"
+              className={styles["TransferFilter-checkbox"]}
+              type="checkbox"
+              onChange={onChangeTransfers}
+              checked={filterIsNoTransfers}
+            />
             <span>Без пересадок</span>
           </label>
         </li>
         <li className={styles["TransferFilter-item"]}>
           <label className={styles["TransferFilter-label"]}>
-            <input className={styles["TransferFilter-checkbox"]} type="checkbox" />
+            <input
+              id="1_TRANSFER"
+              className={styles["TransferFilter-checkbox"]}
+              type="checkbox"
+              onChange={onChangeTransfers}
+              checked={filterIsOneTransfer}
+            />
             <span>1 пересадка</span>
           </label>
         </li>
         <li className={styles["TransferFilter-item"]}>
           <label className={styles["TransferFilter-label"]}>
-            <input className={styles["TransferFilter-checkbox"]} type="checkbox" />
+            <input
+              id="2_TRANSFERS"
+              className={styles["TransferFilter-checkbox"]}
+              type="checkbox"
+              onChange={onChangeTransfers}
+              checked={filterIsTwoTransfers}
+            />
             <span>2 пересадки</span>
           </label>
         </li>
         <li className={styles["TransferFilter-item"]}>
           <label className={styles["TransferFilter-label"]}>
-            <input className={styles["TransferFilter-checkbox"]} type="checkbox" />
+            <input
+              id="3_TRANSFERS"
+              className={styles["TransferFilter-checkbox"]}
+              type="checkbox"
+              onChange={onChangeTransfers}
+              checked={filterIsThreeTransfers}
+            />
             <span>3 пересадки</span>
           </label>
         </li>
